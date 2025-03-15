7165b94c91d50071f99a46059da3c0bc46bc98c0
kartik-667 <vermakartik667@gmail.com>
2025-03-16 00:43:52 +0530

    some backend changes

--- Backend/routes/book.route.js
+++ Backend/routes/book.route.js
@@ -1,9 +1,22 @@
 const express=require('express')
 const bookcontroller=require('../controller/book.controller')
 const router=express.Router()
+const bookmodel=require('../models/bookmodel')
 
 router.get('/',bookcontroller)
 
+
+router.get('/:idn',async (req,res)=>{
+    let targetid=req.params.idn
+
+    const data=await bookmodel.find({id:Number(targetid)})
+
+    if(!data) return res.status(404).json("data not found")
+
+   return  res.status(200).json(data)
+
+
+})
 
 
 
